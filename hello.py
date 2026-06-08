3f2b6f23989c8aa6db3d07599073db50d96badc6
Mardaus Sébastien <norothiais@gmail.com>
2026-06-08 16:14:17 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from pymupdf import message
 init()
  
 def display():
-    message = f"{Fore.GREEN}Ceci est une {Fore.CYAN}modification{Style.RESET_ALL}"
+    message = f"{Fore.GREEN}Ceci est une {Fore.CYAN}modification{Style.RESET_ALL}" # Ceci est un test stash
     print(message)
  
  
