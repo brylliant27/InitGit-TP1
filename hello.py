04c14697e0e7b124c9c5d92aaff3e1b774aec0fe
Sumith07 <harmalkarsumith@gmail.com>
2026-06-08 15:35:10 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+from colorama import init, Fore, Style
+from pymupdf import message
+init()
+
+def display():
+    message = f"{Fore.GREEN}Bonjour les {Fore.CYAN}FI1{Style.RESET_ALL}"
+    print(message)
+
+
+if __name__ == "__main__":
+    display()
